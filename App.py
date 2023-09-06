d8ebdb2ed7a7588d7997a003188b9d303f5c42b1
Quannggg <nhieuvai@gmail.com>
2023-09-06 16:19:49 +0700

    Fix db error

--- App.py
+++ App.py
@@ -1,18 +1,45 @@
 from flask import Flask, render_template, request
-from TestCrawl import extract_information
+from sqlalchemy import create_engine, or_
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.inspection import inspect
+from models import Article,Base  
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+# Configure the database connection
+db_engine = create_engine('sqlite:///information.db')
+Session = sessionmaker(bind=db_engine)
 
+@app.route('/', methods=['POST', 'GET'])
+def Scrawl():
     if request.method == 'POST':
-        search_term = request.form['search_term']
-        url = f"https://jiem.ftu.edu.vn/index.php/jiem/issue/archive?q={search_term}"
-        data = extract_information(url)
-        return render_template('results.html', data=data, search_term=search_term)
-    return render_template('index.html')
+        search = request.form.get('search')  # Use 'search' as the form field name
+        print(search)
+        
+        if search:
+            session = Session()
+            articles = []
+
+            # Get the list of column names in the Article model
+            mapper = inspect(Article)
+            print(mapper)
+            columns = [c.key for c in mapper.attrs]
+            print(columns)
+
+            # Create a list of conditions to search across all columns
+            search_conditions = [getattr(Article, col).like(f'%{search}%') for col in columns]
+            print(search_conditions)
+
+            # Combine conditions with OR to search in any column
+            query_condition = or_(*search_conditions)
+            print(query_condition)
+
+            # Perform the query using the combined condition
+            articles = session.query(Article).filter(query_condition).all()
+            # print(articles)
+
+            return render_template("Home.html", articles=articles, search=search)
+    return render_template("Home.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
-
+    app.run(debug=True, port=8000)
